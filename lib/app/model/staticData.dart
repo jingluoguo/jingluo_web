9108547218c7045a1fc0f0cd49a908b367ca67f6
jingluoguo <55196987+jingluoguo@users.noreply.github.com>
2023-11-02 00:42:25 +0800

    调整文章页面

--- lib/app/model/staticData.dart
+++ lib/app/model/staticData.dart
@@ -2,8 +2,8 @@ class StaticData {
   static List<Map<String, dynamic>> articles = [
     {
       "id": 0,
-      "name": "test1",
-      "link": "www.baidu.com",
+      "name": "iOS蓝牙4.0 开发基本使用",
+      "link": "https://giddy-land-a84.notion.site/iOS-4-0-d7b5b3becff343d587b47d126dd47639?pvs=4",
       "img": "https://github.com/jingluoguo/jingluo_icloud_storage/raw/master/preview/userdefaults.gif",
       "tag": "flutter",
       "author": "JingluoGuo",
--- lib/app/modules/articles/articles_page.dart
+++ lib/app/modules/articles/articles_page.dart
@@ -5,6 +5,7 @@ import 'package:jingluo_web/app/model/article.dart';
 import 'package:jingluo_web/app/modules/articles/articles_controller.dart';
 import 'package:jingluo_web/app/theme/utils/export.dart';
 import 'package:jingluo_web/app/theme/utils/size_extension.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ArticlesPage extends BasePage<ArticlesController> {
   const ArticlesPage({super.key});
@@ -23,7 +24,7 @@ class ArticlesPage extends BasePage<ArticlesController> {
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              childAspectRatio: 1.6,
+              childAspectRatio: 1.2,
               crossAxisSpacing: 12.dp,
               mainAxisSpacing: 12.dp,
               crossAxisCount: isSmallScreen ? 1 : 3,
@@ -39,7 +40,13 @@ class ArticlesPage extends BasePage<ArticlesController> {
   Widget _buildItem(BuildContext ctx, index) {
     Article article = controller.allArticles.value[index];
     return GestureDetector(
-      onTap: () => print("xxxxx${article.link}"),
+      onTap: () async {
+        if (article.link == null) return;
+        var url = Uri.parse(article.link!);
+        if (await canLaunchUrl(url)) {
+          await launchUrl(url);
+        }
+      },
       child: MouseRegion(
         onEnter: (event) {
           controller.nowHover.value = index;
@@ -49,38 +56,70 @@ class ArticlesPage extends BasePage<ArticlesController> {
         },
         cursor: SystemMouseCursors.click,
         child: Obx(() => Container(
-          decoration: BoxDecoration(
-              color: controller.nowHover.value == index
-                  ? Colors.white.withOpacity(0.2)
-                  : Colors.red,
-              borderRadius: BorderRadius.circular(4.dp)),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(child: SizedBox(
-                width: double.infinity,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(4.dp), // 设置圆角半径
-                  child: Image.asset(R.avatar, fit: BoxFit.cover,), // 加载图片
-                ),
-              )),
-              Text(
-                article.name!,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.sp
+              decoration: BoxDecoration(
+                  color: controller.nowHover.value == index
+                      ? Colors.white.withOpacity(0.2)
+                      : Colors.transparent,
+                  borderRadius: BorderRadius.circular(4.dp)),
+              child: Padding(
+                padding: EdgeInsets.all(2.dp),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(padding: EdgeInsets.only(bottom: 2.dp), child: SizedBox(
+                      child: Text(
+                        "${article.name}",
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(color: Colors.white, fontSize: 14.sp),
+                      ),
+                    ),),
+                    Expanded(
+                        child: Stack(
+                      children: [
+                        SizedBox(
+                          width: double.infinity,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(4.dp), // 设置圆角半径
+                            child: Image.asset(
+                              R.avatar,
+                              fit: BoxFit.cover,
+                            ), // 加载图片
+                          ),
+                        ),
+                        Positioned(
+                            right: 2.dp,
+                            top: 2.dp,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(4.dp),
+                              ),
+                              child: Center(
+                                child: Padding(
+                                  padding: EdgeInsets.all(2.dp),
+                                  child: Text("${article.tag}"),
+                                ),
+                              ),
+                            ))
+                      ],
+                    )),
+                    SizedBox(height: 2.dp),
+                    Row(
+                      children: [
+                        const Expanded(child: SizedBox()),
+                        Text(
+                          article.date!,
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.4),
+                              fontSize: 8.sp),
+                        )
+                      ],
+                    )
+                  ],
                 ),
               ),
-              Text(
-                article.date!,
-                style: TextStyle(
-                    color: Colors.white.withOpacity(0.4),
-                    fontSize: 6.sp
-                ),
-              )
-            ],
-          ),
-        )),
+            )),
       ),
     );
   }
